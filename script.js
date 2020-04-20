8ff56eed2472dbf76ff415eba9eb33b8bc591ae7
mikell17 <losova.pavla@gmail.com>
2020-04-20 19:38:21 +0200

    Week3Day1

---
+++ script.js
@@ -0,0 +1,3 @@
+$('.img').append('<img class="img" id="img1" src="./images/P1310767.jpg" />');
+$('#arrowL').append('<img id="#arrowL" src="./arrow-left.svg" />');
+$('#arrowR').append('<img id="#arrowR" src="./arrow-left.svg" />');
